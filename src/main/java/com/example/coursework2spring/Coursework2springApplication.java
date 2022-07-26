4caf975667b4d5820d94e2ce204511c1497f4ed3
Dmitry <leon1861@yandex.ru>
2022-07-26 16:07:02 +0700

    Initial commit

---
+++ src/main/java/com/example/coursework2spring/Coursework2springApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursework2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework2springApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coursework2spring/Coursework2springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework2spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
