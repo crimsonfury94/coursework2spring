7cec8685a1c73594520fb059b27ab12e2ab46ac9
Dmitry <leon1861@yandex.ru>
2022-07-28 17:08:54 +0700

    later

--- src/main/java/com/example/coursework2spring/controller/JavaQuestionController.java
+++ src/main/java/com/example/coursework2spring/controller/JavaQuestionController.java
@@ -1,7 +1,35 @@
 package com.example.coursework2spring.controller;
 
+import com.example.coursework2spring.data.Question;
+import com.example.coursework2spring.impl.JavaQuestionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
 @RestController
+@RequestMapping(path = "/exam/java")
 public class JavaQuestionController {
+
+    private final JavaQuestionService javaQuestionService;
+
+    public JavaQuestionController(JavaQuestionService javaQuestionService) {
+        this.javaQuestionService = javaQuestionService;
+    }
+
+    @GetMapping(path = "/add")
+    public Question add(String question, String answer) {
+        return javaQuestionService.add(question, answer);
+    }
+
+    @GetMapping(path = "/remove")
+    public Question remove(Question question) {
+        return javaQuestionService.remove(question);
+    }
+
+    @GetMapping(path = "/find")
+    public Collection<Question> getAll() {
+        return javaQuestionService.getAll();
+    }
 }
--- src/main/java/com/example/coursework2spring/impl/JavaQuestionService.java
+++ src/main/java/com/example/coursework2spring/impl/JavaQuestionService.java
@@ -5,31 +5,46 @@ import com.example.coursework2spring.service.QuestionService;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
 public class JavaQuestionService implements QuestionService {
 
-    private Set<Question> questions;
+    private static final int LIMIT = 5;
+
+    private final Set<Question> questions;
+
+    public JavaQuestionService() {
+        this.questions = new HashSet<>();
+    }
 
     @Override
     public Question add(String question, String answer) {
-        return null;
+        Question addQuestion = new Question(question, answer);
+        if (questions.size() < LIMIT) {
+            questions.add(addQuestion);
+            return addQuestion;
+        }
+        throw new ArrayStoreException();
     }
 
     @Override
     public Question add(Question question) {
-        return null;
+        questions.add(question);
+        return question;
     }
 
     @Override
     public Question remove(Question question) {
-        return null;
+        questions.remove(question);
+        return question;
     }
 
     @Override
     public Collection<Question> getAll() {
-        return null;
+        return Collections.unmodifiableCollection(questions);
     }
 
     @Override
